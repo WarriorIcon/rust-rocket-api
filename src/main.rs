fe6e689d8a6dba1a8cf161f94d2d5f2ee2508482
WarriorIcon <66060955+WarriorIcon@users.noreply.github.com>
2023-03-19 01:32:39 -0400

    CRUD routes wip

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,38 @@
 #[macro_use] extern crate rocket;
+use rocket::serde::json::{Value, json};
+use rocket::response::status;
 
-#[get("/")]
-fn hello() -> &'static str {
-    "Hello, world!\n"
+#[get("/rustaceans")]
+fn get_rustaceans() -> Value {
+    json!([{ "id": 1, "name": "John Doe" }, { "id": 2, "name": "John Doe again" }])
+}
+#[get("/rustaceans/<id>")]
+fn view_rustacean(id: i32) -> Value {
+    json!({"id": id, "name": "John Doe", "email": "john@doe.com"})
+}
+#[post("/rustaceans", format = "json")]
+fn create_rustacean() -> Value {
+    json!({"id": 3, "name": "John Doe", "email": "john@doe.com"})
+}
+#[put("/rustaceans/<id>", format = "json")]
+fn update_rustacean(id: i32) -> Value {
+    json!({"id": id, "name": "John Doe", "email": "john@doe.com"})
+}
+#[delete("/rustaceans/<_id>")]
+fn delete_rustacean(_id: i32) -> status::NoContent {
+    status::NoContent
 }
 
 #[rocket::main]
 async fn main() {
     let _ = rocket::build()
-        .mount("/", routes![hello])
+        .mount("/", routes![
+            get_rustaceans,
+            view_rustacean,
+            create_rustacean,
+            update_rustacean,
+            delete_rustacean
+        ])
         .launch()
         .await;
 }
